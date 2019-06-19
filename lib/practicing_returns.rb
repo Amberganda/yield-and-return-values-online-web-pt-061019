8d83cabbaf103dee92d2b0133354a9f589cbbb2c
Amber Taranto <ambzzzz@gmail.com>
2019-06-19 04:26:45 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,10 +4,10 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    binding.pry
+    # binding.pry
     collection << yield(array[i])
     i += 1
   end
 end
 #binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+puts hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
